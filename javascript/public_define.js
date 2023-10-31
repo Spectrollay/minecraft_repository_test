c85309ff1fb9d8825cd3c9658a45028a31d288b3
Spectrollay <2819288472@qq.com>
2023-10-31 17:28:20 +0800

    测试

--- javascript/public_define.js
+++ javascript/public_define.js
@@ -9,12 +9,15 @@ const texts = {
     preview_btn1: "更新历史",
     preview_btn2: "<img class=\"link_img\" src=\"\" alt=\"link\"/>提出反馈",
     page_info_title1: "INFORMATION",
-    page_info_detail1: "Version: 4.3-Preview1<br>Server Version: 4.0<br>Updated: 2023-10-31-01",
+    page_info_detail1: "Version: 4.3-Preview2<br>Server Version: 4.0<br>Updated: 2023-10-31-08",
     page_info_title2: "ABOUT US",
     page_info_detail2: "<span>Developer: @Spectrollay<br>Maintainer: @Spectrollay<br>Chat Group: [<a href=\"https://t.me/Spectrollay_MCW\" target=\"_blank\" onclick=\"playSound1();\">Telegram</a>] [<a href=\"https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=WVA6aPqtv99hiYleW7vUq5OsBIufCAB1&authKey=B0%2BaXMCTqnmQrGh0wzCZTyWTIPyHS%2FPEM5QXcFfVwroFowNnzs6Yg1er1%2F8Fekqp&noverify=0&group_code=833473609\" target=\"_blank\" onclick=\"playSound1();\">QQ</a>] [<a href=\"https://yhfx.jwznb.com/share?key=VyTE7W7sLwRl&ts=1684642802\" target=\"_blank\" onclick=\"playSound1();\">云湖</a>]<span>",
     jump_text: "点击前往下载页面",
     back_to_main: "返回首页",
+    sidebar_bottom_title: "Minecraft Kit",
+    sidebar_bottom_detail1: "© 2020 Spectrollay",
     sidebar_bottom_btn: "官方网站",
+    minecraft_wiki: "中文Minecraft Wiki",
     download_btn1: "官方原版",
     download_btn1_1: "<img class=\"link_img_black\" src=\"\" alt=\"link\"/>官方原版(外部链接)",
     download_btn2: "中文译名修正",
@@ -66,7 +69,9 @@ const buttons = document.querySelectorAll('.btn');
 
 function updateButtonText(button) {
     const textKey = button.getAttribute('text-generation');
-    button.innerHTML = texts[textKey];
+    if (textKey !== null) {
+        button.innerHTML = texts[textKey];
+    }
 }
 
 buttons.forEach(button => {
@@ -77,6 +82,14 @@ const editionBlocks = document.getElementsByClassName("edition_block");
 if (editionBlocks) {
     for (let i = 0; i < editionBlocks.length; i++) {
         editionBlocks[i].innerHTML = texts.jump_text;
+    }
+} else {
+}
+
+const wikiTexts = document.getElementsByClassName("wiki");
+if (wikiTexts) {
+    for (let i = 0; i < wikiTexts.length; i++) {
+        wikiTexts[i].innerHTML = texts.minecraft_wiki;
     }
 } else {
 }
@@ -101,6 +114,8 @@ setElementText("page_info_title1", texts.page_info_title1);
 setElementText("page_info_detail1", texts.page_info_detail1);
 setElementText("page_info_title2", texts.page_info_title2);
 setElementText("page_info_detail2", texts.page_info_detail2);
+setElementText("sidebar_bottom_title", texts.sidebar_bottom_title);
+setElementText("sidebar_bottom_detail1", texts.sidebar_bottom_detail1);
 setElementText("sidebar_bottom_btn", texts.sidebar_bottom_btn);
 setElementText("preview_title", texts.preview_title);
 setElementText("preview_detail1", texts.preview_detail1);
