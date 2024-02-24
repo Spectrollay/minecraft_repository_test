7fbf2b7c7c4d28bd8055413fbb7dcab85a8f4543
Spectrollay <2819288472@qq.com>
2024-02-24 19:28:24 +0800

    2024022401

--- javascript/advanced.js
+++ javascript/advanced.js
@@ -1,18 +1,27 @@
 // 切换开关
-const switchElement = document.getElementById("switch_exp");
-const switch_exp_status = document.getElementById("switch_exp_status");
-let isOn = false;
+const switchElement = document.getElementsByClassName("switch");
+const toggleSlider = document.getElementsByClassName("toggle_slider");
+const switch_status = document.getElementsByClassName("switch_status");
 
 // 添加点击事件监听器
-switchElement.addEventListener("click", function () {
-    isOn = !isOn;
-    playSound1();
-    switchElement.classList.toggle("on", isOn);
-    if (isOn) {
-        switch_exp_status.textContent = "开关: 开启";
-        console.log("打开开关");
-    } else {
-        switch_exp_status.textContent = "开关: 关闭";
-        console.log("关闭开关");
-    }
-});
+for (let i = 0; i < switchElement.length; i++) {
+    let isOn = switchElement[i].classList.contains("on");
+    switchElement[i].addEventListener("click", function () {
+        isOn = !isOn;
+        playSound1();
+        switchElement[i].classList.toggle("on", isOn);
+        switchElement[i].classList.toggle("off", !isOn);
+        if (isOn) {
+            toggleSlider[i].classList.add('toggle_bounce_left');
+            toggleSlider[i].classList.remove('toggle_bounce_right');
+            switch_status[i].textContent = "开关: 开启";
+            console.log("打开开关");
+        } else {
+            toggleSlider[i].classList.add('toggle_bounce_right');
+            toggleSlider[i].classList.remove('toggle_bounce_left');
+            switch_status[i].textContent = "开关: 关闭";
+            console.log("关闭开关");
+        }
+    });
+}
+
