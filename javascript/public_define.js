eb14fa73d7d3eb726a2c6fee58e22ea9fcde6d79
Spectrollay <2819288472@qq.com>
2024-05-04 15:05:05 +0800

    2024050401

--- javascript/public_define.js
+++ javascript/public_define.js
@@ -5,12 +5,12 @@ const texts = {
     preview_btn1: "开发日志",
     preview_btn2: "<img class=\"link_img\" src=\"\" alt=\"\"/>提出反馈",
     page_info_title1: "INFORMATION",
-    page_info_detail1: "<span>Version: 4.6.2.32.Canary<br>Server Version: 4.0<br>Updated: 2024-05-03-01</span>",
+    page_info_detail1: "<span>Version: 4.6.2.33.Canary<br>Server Version: 4.0<br>Updated: 2024-05-04-01</span>",
     page_info_title2: "ABOUT US",
     page_info_detail2: "<span>Developer: @Spectrollay<br>Maintainer: @Spectrollay<br>Chat Group: [<a href=\"https://t.me/Spectrollay_MCW\" target=\"_blank\" onclick=\"playSound1();\">Telegram</a>] [<a href=\"https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=WVA6aPqtv99hiYleW7vUq5OsBIufCAB1&authKey=B0%2BaXMCTqnmQrGh0wzCZTyWTIPyHS%2FPEM5QXcFfVwroFowNnzs6Yg1er1%2F8Fekqp&noverify=0&group_code=833473609\" target=\"_blank\" onclick=\"playSound1();\">QQ</a>] [<a href=\"https://yhfx.jwznb.com/share?key=VyTE7W7sLwRl&ts=1684642802\" target=\"_blank\" onclick=\"playSound1();\">云湖</a>]<span>",
     page_info_title3: "MADE WITH ❤️ IN CHINA",
     jump_text: "点击前往下载页面",
-    back_to_main: "返回首页",
+    // back_to_main: "返回首页",
     sidebar_bottom_title: "Minecraft Kit",
     sidebar_bottom_detail1: "© 2020 Spectrollay",
     sidebar_bottom_btn: "官方网站",
@@ -241,13 +241,13 @@ if (wikiTexts) {
 } else {
 }
 
-const backToMainTexts = document.getElementsByClassName("back_to_main");
-if (backToMainTexts) {
-    for (let i = 0; i < backToMainTexts.length; i++) {
-        backToMainTexts[i].innerHTML = texts.back_to_main;
-    }
-} else {
-}
+// const backToMainTexts = document.getElementsByClassName("back_to_main");
+// if (backToMainTexts) {
+//     for (let i = 0; i < backToMainTexts.length; i++) {
+//         backToMainTexts[i].innerHTML = texts.back_to_main;
+//     }
+// } else {
+// }
 
 const setElementText = (elementId, text) => {
     const element = document.getElementById(elementId);
