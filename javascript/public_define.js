8914caa6e99c1c3767567a6778c4d5c5cb0de922
Spectrollay <2819288472@qq.com>
2024-05-16 22:14:33 +0800

    2024051601

--- javascript/public_define.js
+++ javascript/public_define.js
@@ -1,14 +1,14 @@
 // 版本变量
 // TODO 需在每次提交前检查
 const primary_version_name = "4.6"; // 例 4.0
-const secondary_version_name = primary_version_name + ".3"; // 例 4.0.0
-const version_name_short = secondary_version_name + ".49"; // 例 4.0.0.1
+const secondary_version_name = primary_version_name + ".4"; // 例 4.0.0
+const version_name_short = secondary_version_name + ".50"; // 例 4.0.0.1
 const version_type = "Canary"; // Preview/Insider_(Preview/Alpha/Beta)/Canary/Alpha/Beta/Pre/RC/Release/SP
 const version_type_count = version_type + "4"; // 例 Build1
 const version_name = version_name_short + "." + version_type; // 例 4.0.0.1.Build
 const version_nickname = secondary_version_name + "-" + version_type_count; // 例 4.0.0-Build1
 const server_version = "4.0";
-const update_count = "2024-05-15-01";
+const update_count = "2024-05-16-01";
 let commit = "#"; // 例 #2024010101 , 仅留 # 则从 update_count 提取
 if (commit === "#") {
     commit = "#" + update_count.replace(/-/g, "");
