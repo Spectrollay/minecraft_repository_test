08ba59e0a098d2afd73508a807a9cb3c9d735395
Spectrollay <2819288472@qq.com>
2024-07-25 10:47:36 +0800

    2024072401

--- javascript/custom_elements.js
+++ javascript/custom_elements.js
@@ -97,14 +97,14 @@ class CustomCheckbox extends HTMLElement {
         if (isChecked) {
             this.setAttribute('active', 'off');
             console.log("关闭复选框", this.id);
-            if (this.classList.contains('neverShowIn7Days')) {
-                localStorage.removeItem(`(/minecraft_repository_test/)neverShowIn7Days`);
+            if (this.classList.contains('neverShowIn15Days')) {
+                localStorage.removeItem(`(/minecraft_repository_test/)neverShowIn15Days`);
             }
         } else {
             this.setAttribute('active', 'on');
             console.log("打开复选框", this.id);
-            if (this.classList.contains('neverShowIn7Days')) {
-                localStorage.setItem(`(/minecraft_repository_test/)neverShowIn7Days`, Date.now().toString());
+            if (this.classList.contains('neverShowIn15Days')) {
+                localStorage.setItem(`(/minecraft_repository_test/)neverShowIn15Days`, Date.now().toString());
             }
         }
 
